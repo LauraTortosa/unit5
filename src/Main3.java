3659715803571307a20b9723d00260d89bc495a3
LauraTortosa <alutor9252@ieselcaminas.org>
2022-11-06 16:53:46 +0100

    punto 22 Laura Tortosa

---
+++ src/Main3.java
@@ -0,0 +1,9 @@
+public class Main3 {
+    public static void main(String[] args) {
+        Segment seg1 = new Segment(new Point(4,5), new Point(6,8));
+        seg1.setOffset(4,4);
+        System.out.println(seg1.toString());
+    }
+
+
+}
