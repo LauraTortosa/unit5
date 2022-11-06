66b79edcb33e64d067fa7d4383176c9c731ace32
LauraTortosa <alutor9252@ieselcaminas.org>
2022-11-06 16:47:10 +0100

    punto 21 Laura Tortosa

--- src/Segment.java
+++ src/Segment.java
@@ -25,9 +25,11 @@ public class Segment {
         endPoint.setOffset(offX, offY);
     }
 
+    public void setStartPoint(Point startPoint) { //modificar
+        this.startPoint = startPoint;
+    }
 
-
-
-
-
+    public void setEndPoint(Point endPoint) {
+        this.endPoint = endPoint;
+    }
 }
