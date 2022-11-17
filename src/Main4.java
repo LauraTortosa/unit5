350827e76c48551259384e96147283ec0f4b29f0
LauraTortosa <alutor9252@ieselcaminas.org>
2022-11-17 08:31:12 +0100

    32 Laura Tortosa

---
+++ src/Main4.java
@@ -0,0 +1,10 @@
+public class Main4 {
+    public static void main(String[] args) {
+        Rectangle rect1 = new Rectangle();
+        System.out.println(rect1);
+        rect1.moveTo(4,7);
+        System.out.println(rect1);
+
+
+    }
+}
